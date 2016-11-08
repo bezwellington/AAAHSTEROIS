26c476a73baba4b22f973dfbd0afd19883d501a8
Wellington Bezerra <bezwellington@gmail.com>
2016-11-08 18:19:28 -0200

    Có

--- AAAHSTEROIS/GameViewController.swift
+++ AAAHSTEROIS/GameViewController.swift
@@ -12,13 +12,7 @@ import GameplayKit
 import GameController
 
 class GameViewController: UIViewController {
-    /*
-        var originalPanViewCenter: CGPoint?
-        var panViewConstraintCenterX: NSLayoutConstraint!
-        var panViewConstraintCenterY: NSLayoutConstraint!
- */
  
-
     override func viewDidLoad() {
         super.viewDidLoad()
         let skView = view as! SKView
@@ -30,36 +24,11 @@ class GameViewController: UIViewController {
         skView.showsNodeCount = true
         skView.presentScene(scene)
         
-        /*
-        originalPanViewCenter = CGPoint(x: panViewConstraintCenterX.constant, y: panViewConstraintCenterY.constant)
-        
-        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: "userPanned:")
-        skView.addGestureRecognizer(panGestureRecognizer)
- */
-
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-    
-    // MARK: UIPanGestureRecognizer
-/*
-    func userPanned(panGestureRecognizer : UIPanGestureRecognizer) {
-        let translation = panGestureRecognizer.translation(in: self.view)
-        print(translation)
-        guard let originalCenter = originalPanViewCenter else { return }
-        panViewConstraintCenterX.constant = originalCenter.x
-        panViewConstraintCenterY.constant = originalCenter.y
-        
-        if (panGestureRecognizer.state == .changed) {
-            panViewConstraintCenterX.constant += translation.x
-            panViewConstraintCenterY.constant += translation.y
-        }
-    }
- */
-    
-    
     
 }
