d6606125a8e6b464a686004cb08cccbb1aa3ee5f
Wellington Bezerra <bezwellington@gmail.com>
2016-11-09 19:57:57 -0200

    Shake do Remote Control

--- AAAHSTEROIS/GameViewController.swift
+++ AAAHSTEROIS/GameViewController.swift
@@ -14,61 +14,87 @@ import GameController
 
 class GameViewController: UIViewController {
     
+    @IBOutlet weak var earthView: EarthView!
+    
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
-    
-     var codeView: VerificationCodeView!
+    var codeView: VerificationCodeView!
     var gameScene = GameScene()
+    let deadlineTime = DispatchTime.now()
     
-
-  @IBOutlet weak var earthView: EarthView!
-  
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    //codeView = VerificationCodeView(frame: self.view.bounds, verificationCode: "1234")
-    //self.view.addSubview(codeView)
-    
-    appDelegate.mpcManager.delegate = self
-    appDelegate.mpcManager.enableServices(enable: true)
-    
-    
-    loadGameScene()
-  }
-  
-  func loadGameScene(){
-    let skView = view as! SKView
-    
-    //let scene = SKScene(fileNamed: "GameScene")
-    gameScene.size = view.bounds.size
-    gameScene.scaleMode = .aspectFill
-    skView.ignoresSiblingOrder = true
-    skView.showsFPS = true
-    skView.showsNodeCount = true
-    skView.presentScene(gameScene)
-    
-    earthView.load3DEarth()
-    
-  }
-  
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Release any cached data, images, etc that aren't in use.
-  }
-  
-  
-  override func pressesBegan(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
-    for item in presses {
-      if item.type == .select {
-        gameScene.count -= 1
-        print("Count = \(gameScene.count)")
-      }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //codeView = VerificationCodeView(frame: self.view.bounds, verificationCode: "1234")
+        //self.view.addSubview(codeView)
+        
+        appDelegate.mpcManager.delegate = self
+        appDelegate.mpcManager.enableServices(enable: true)
+        
+        loadGameScene()
+        
+        //Função que começa a pegar a aceleração do Remote Control
+        startControllerAcceleration()
+        
     }
-    if(gameScene.count == 2){
-      print("voce precisa recarregar sua arma")
+    
+    func loadGameScene(){
+        let skView = view as! SKView
+        
+        gameScene.size = view.bounds.size
+        gameScene.scaleMode = .aspectFill
+        skView.ignoresSiblingOrder = true
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.presentScene(gameScene)
+        
+        earthView.load3DEarth()
+        
     }
-  }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    override func pressesBegan(_ presses: Set<UIPress>, with event: UIPressesEvent?) {
+        for item in presses {
+            if item.type == .select {
+                gameScene.count -= 1
+                print("Count = \(gameScene.count)")
+            }
+        }
+        if(gameScene.count == 2){
+            print("voce precisa recarregar sua arma")
+        }
+    }
+    
+    func startControllerAcceleration() {
+        DispatchQueue.main.asyncAfter(deadline: deadlineTime, execute: {
+            self.atualiza()
+        })
+    }
+    
+    func atualiza() {
+        GCController.controllers().first?.motion?.valueChangedHandler = { motion in
+            let realAcceleration = motion.userAcceleration
+            if (realAcceleration.x > 5 || realAcceleration.y > 5 || realAcceleration.z > 5)
+            {
+                print("Arma recarregada com sucesso!")
+                //print ("Aceleração real! \(realAcceleration)")
+                //print ("Aceleração x! \(realAcceleration.x)")
+                //print ("Aceleração y! \(realAcceleration.y)")
+                //print ("Aceleração z! \(realAcceleration.z)")
+                self.reloadWeapon()
+            }
+        }
+        
+    }
+    
+    // Função que recarrega arma
+    func reloadWeapon(){
+        self.gameScene.count += 1
+    }
+    
 }
 
 
